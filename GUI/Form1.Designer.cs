4cf1ff156b1a19ad8638cecf6c96585e7ca5b88f
Branko Rados <BRados@dspace.hr>
2021-11-04 19:08:36 +0100

    adding plot

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -30,23 +30,37 @@ namespace Vsite.Csharp.KvadartnaJednadzba.Gui
         private void InitializeComponent()
         {
             this.resultDisplay1 = new Vsite.Csharp.KvadartnaJednadzba.Gui.ResultDisplay();
+            this.functionPanel1 = new Vsite.Csharp.KvadartnaJednadzba.Gui.FunctionPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.functionPanel1)).BeginInit();
             this.SuspendLayout();
             // 
             // resultDisplay1
             // 
             this.resultDisplay1.Location = new System.Drawing.Point(21, 12);
             this.resultDisplay1.Name = "resultDisplay1";
-            this.resultDisplay1.Size = new System.Drawing.Size(784, 489);
+            this.resultDisplay1.Size = new System.Drawing.Size(437, 489);
             this.resultDisplay1.TabIndex = 0;
+            // 
+            // functionPanel1
+            // 
+            this.functionPanel1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.functionPanel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.functionPanel1.Location = new System.Drawing.Point(464, 12);
+            this.functionPanel1.Name = "functionPanel1";
+            this.functionPanel1.Size = new System.Drawing.Size(517, 520);
+            this.functionPanel1.TabIndex = 1;
+            this.functionPanel1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(832, 476);
+            this.ClientSize = new System.Drawing.Size(993, 562);
+            this.Controls.Add(this.functionPanel1);
             this.Controls.Add(this.resultDisplay1);
             this.Name = "Form1";
             this.Text = "Quadratic Equation";
+            ((System.ComponentModel.ISupportInitialize)(this.functionPanel1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -54,6 +68,7 @@ namespace Vsite.Csharp.KvadartnaJednadzba.Gui
         #endregion
 
         private ResultDisplay resultDisplay1;
+        private FunctionPanel functionPanel1;
     }
 }
 
